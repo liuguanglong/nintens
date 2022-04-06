d98374756c56cc61427676b81495529d12ecb01b
liugu <guanglong.liu@inossem.com>
2022-04-06 15:19:46 -0400

    修改FormController，不需要注册ControlName和EventName

--- uiscriptengine/FormController.cs
+++ uiscriptengine/FormController.cs
@@ -12,19 +12,6 @@ namespace uiscriptengine
 
         Dictionary<String, String> dicEventHanlder = new Dictionary<string, string>();
 
-        public void addEvent(String controlName,String eventName,String functionName)
-        {
-            String key = $"{controlName}.{eventName}";
-            if(dicEventHanlder.ContainsKey(key))
-            {
-                dicEventHanlder[key] = functionName;
-            }
-            else
-            {
-                dicEventHanlder.Add(key, eventName);
-            }
-        }
-
         public void RegisterEventHanlder(BaseEventHanlder hanlder)
         {
             this.eventHanlder = hanlder;
